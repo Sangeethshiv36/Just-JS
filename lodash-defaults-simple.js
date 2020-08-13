ea268c59248779f907c47bc27b28f8d5d7ac02a8
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-13 20:58:33 +0530

    lodash defaults minimal functionality

---
+++ lodash-defaults-simple.js
@@ -0,0 +1,25 @@
+const _ = {}
+
+const newlyDiscoveredWeapons =  {
+  "Lead Pipe": true,
+  Rope: true,
+  Wrench: true,
+  Revolver: false
+}
+
+const discoveredWeapons = {
+  Revolver: true
+}
+
+_.defaults = (object,source) => {
+  Object.keys(source).forEach((prop)=> {
+    if(object[prop] === undefined) {
+      object[prop] = source[prop]
+    }
+  });
+  return object;
+}
+
+const weaponsTracked = _.defaults(discoveredWeapons,newlyDiscoveredWeapons)
+
+console.log(weaponsTracked)
