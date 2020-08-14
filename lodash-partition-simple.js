5ed70ee1fac7c37d05ec4720d8c1d9c921b72b96
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-14 06:25:19 +0530

    lodash parition minimal

---
+++ lodash-partition-simple.js
@@ -0,0 +1,18 @@
+// iterator vs predicate - something to keep in mind.
+// A simple function to parititon odd and even numbers in separate array
+const _ = {};
+
+_.partition = (list,predicate) => {
+	const result = [[],[]];
+
+	list.forEach((item)=> {
+		if(predicate(item)) {
+			result[0].push(item);
+		}else {
+			result[1].push(item);
+		}
+	});
+	return result;
+}
+
+_.partition([1,2,3,4,5,6],n => n % 2);
