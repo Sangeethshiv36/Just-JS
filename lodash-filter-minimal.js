3841dd8f16006ac3f4b3b62060c34d7578e67dae
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-14 06:57:51 +0530

    lodash filter minimal

---
+++ lodash-filter-minimal.js
@@ -0,0 +1,28 @@
+const _ = {};
+
+const rooms2 = ["Kitchen","Library","Conservatory","Hall","Dining Room"];
+
+_.filter = (collection,predicate) => {
+	let results = [];
+	collection.forEach(item=> {
+		if(predicate(item)){
+			results.push(item);
+		}
+	})
+	return results;
+}
+
+// Tests
+const clues = _.filter(rooms2,room => {
+	return room.length === 7;
+});
+
+console.log(clues.length === 2);
+
+console.log(clues[0] === 'Kitchen');
+console.log(clues[1] === 'Library');
+
+console.log(clues);
+
+
+
