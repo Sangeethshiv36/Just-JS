d698e35bdef4d762b14ee1f83531200a3ffbaf91
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-14 10:47:49 +0530

    refactor lodash transform

--- lodash-transform-simple.js
+++ lodash-transform-simple.js
@@ -39,26 +39,20 @@ const cluesSoFar = {
   Hall: true
 };
 
-_.transform = (object, iteratee, accumulator) => {
-  const collectionType = Array.isArray(object);
+_.transform = (collection, iteratee, accumulator) => {
   let returnValueIt = true;
 
-  if (collectionType) {
-    object.forEach((item, index) => {
-      if (!returnValueIt && returnValueIt !== undefined) {
-        return;
-      }
-      returnValueIt = iteratee(accumulator, item, index);
-    });
-  } else if (!collectionType && typeof object === 'object') {
-    Object.keys(object).forEach((key) => {
-      if (!returnValueIt && returnValueIt !== undefined) {
-        console.log('in');
-        return;
-      }
-      returnValueIt = iteratee(accumulator, object[key], key);
-    });
-  }
+  //in the prev commit i did something stupid, which was i checked whether the 'collection' which we are getting is either object or array and if its arry i used collection.forEach and if its object i did Object.keys(collection).forEach , surprise surprise arrays are also of type object in javascript and it has indices as its key (0,1,2 i.e arr=[1,2,3] will be like {0:1,1:2,2:3} so i don't have to do whatever i did before, i can just simply do what i have done below.)
+
+  // Also something to note here one reason we have an array is because only symbols and strings can be a key (property name ) of an object, numerics can't be but with arrays we can do that and also thats why when accessing object props through values we use the [] notation instead of dot notation.
+
+  Object.keys(collection).forEach((key) => {
+    if (!returnValueIt && returnValueIt !== undefined) {
+      console.log('in');
+      return;
+    }
+    returnValueIt = iteratee(accumulator, collection[key], key);
+  });
   return accumulator;
 };
 
