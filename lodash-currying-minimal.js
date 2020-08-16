b780b28f6278a9547bba6f2a74a80a4346ed73da
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-16 12:41:09 +0530

    added comments to currying func

--- lodash-currying-minimal.js
+++ lodash-currying-minimal.js
@@ -1,19 +1,22 @@
+// Currying is when you break down a function that takes multiple arguments into a series of functions that each take only one argument.
+
+/* _.curry3 = (func) => {
+   return (arg1) => {
+     return (arg2) => {
+      return (arg3) => {
+        return func(arg1, arg2, arg3);
+       };
+    };
+   };
+ };*/
+
 const _ = {};
 
 const whoIsIt = (weapon, room, suspect, curried) => {
   return `It was ${suspect} in the ${room} with a ${weapon} !!! ${curried}`;
 };
 
-// _.curry3 = (func) => {
-//   return (arg1) => {
-//     return (arg2) => {
-//       return (arg3) => {
-//         return func(arg1, arg2, arg3);
-//       };
-//     };
-//   };
-// };
-
+// here is a curry function -  takes function as an argument and return a function, it accepts the first argument of the passed in function and returns a function which accepts the second argument and so on.
 _.curry = (func) => {
   return (curriedFn = (...args) => {
     if (args.length < func.length) {
