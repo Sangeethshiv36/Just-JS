fcd33232d38401b89367f0f4262fbca7a00e3266
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-25 19:58:00 +0530

    immutable obj example

--- daily-js.js
+++ daily-js.js
@@ -9,3 +9,43 @@ for (var i = 0; i < 5; i++) {
 for (let i = 0; i < 5; i++) {
   setTimeout(() => console.log(i)); // prints as expected coz 'let' creates new variable each time.
 }
+
+//------------------------------------------------------------------------------//
+
+// immutability
+
+const user = {
+  firstName: 'sangeeth',
+  lastName: 'sivan',
+  alias: 'berzerk'
+};
+
+const changeName = (obj) => {
+  obj.firstName = 'guts';
+  obj.lastName = 'keyonashi';
+  return obj;
+};
+
+console.log(changeName(user)); // { firstName: 'guts', lastName: 'keyonashi', alias: 'berzerk' }
+
+console.log(user); // { firstName: 'guts', lastName: 'keyonashi', alias: 'berzerk' }
+
+const user1 = {
+  firstName: 'sangeeth',
+  lastName: 'sivan',
+  alias: 'berzerk'
+};
+
+const immutatbleChangeName = (obj) => {
+  // const copy = {...obj}
+  // copy.firstName = 'guts'
+  // copy.lastName = 'keyonashi';
+  return {
+    ...obj,
+    firstName: 'guts',
+    lastName: 'keyonashi'
+  };
+};
+
+console.log(immutatbleChangeName(user1)); // { firstName: 'guts', lastName: 'keyonashi', alias: 'berzerk' }
+console.log(user1); // { firstName: 'sangeeth', lastName: 'sivan', alias: 'berzerk' }
