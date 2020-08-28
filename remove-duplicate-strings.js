f6488e0da3f1c838ce2d0a378e5c8b0a412e5c92
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-08-29 01:38:04 +0530

    added one liner sol for remove duplicates

--- remove-duplicate-strings.js
+++ remove-duplicate-strings.js
@@ -11,7 +11,12 @@ const removeDuplicates = (str) => {
   return resultArr.join(' ');
 };
 
-console.log(removeDuplicates('This is a test test string.'));
+// One liner way handles cases but doesn't retain orginal chars case.
+const removeDuplicates1 = (str) => {
+  return [...new Set(str.toLowerCase().split(' '))].join(' ');
+};
+
+console.log(removeDuplicates1('This is a test test string.'));
 console.log(removeDuplicates('Remove the duplicates Duplicates '));
 console.log(removeDuplicates('Handle case sensitive Case'));
 console.log(removeDuplicates('All is good and well and good '));
