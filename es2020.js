8872db38b57bbf8bb56ebca867d121e1ba16bdd4
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-10-25 20:23:52 +0530

    es2020 examples should be added next

--- es2020.js
+++ es2020.js
@@ -1,1 +1,50 @@
-// This file will have ES2020 features
+/* New in ES2020
+
+	Atomics.wait, Atomics.notify, Atomics.waitAsync 24 September 2020 ECMAScript ES2020
+
+  TODO: Examples
+
+	Nullish coalescing 17 September 2019 ECMAScript ES2020
+
+	TODO: Examples
+
+
+	Optional chaining 27 August 2019 ECMAScript ES2020
+
+	TODO: Examples
+
+
+	globalThis 16 July 2019 ECMAScript ES2020
+
+
+	TODO: Examples
+
+	Promise combinators 12 June 2019 ECMAScript ES2020 ES2021
+
+	TODO: Examples
+
+
+
+	String.prototype.matchAll 02 February 2019 ECMAScript ES2020
+
+	TODO: Examples
+
+
+
+	Module namespace exports 18 December 2018 ECMAScript ES2020
+
+	TODO: Examples
+
+
+
+	BigInt: arbitrary-precision integers in JavaScript 01 May 2018 ECMAScript ES2020
+
+	TODO: Examples
+
+
+	Dynamic import() 21 November 2017 ECMAScript ES2020
+
+	TODO: Examples
+
+
+*/
