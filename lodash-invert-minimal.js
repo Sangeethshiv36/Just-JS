9966bb97f24c917d4fff2dd2c0e1a14963911804
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-10-29 11:52:54 +0530

    lodash-invert logic

--- lodash-invert-minimal.js
+++ lodash-invert-minimal.js
@@ -6,6 +6,20 @@ returns: {'Sangeeth Sivan':'name','25':'age,'true':'has pet}
 
 const _ = {};
 
-_.invert = (obj) => {};
+_.invert = (obj) => {
+  const objClone = obj;
+  const entries = Object.entries(obj);
+
+  const invertedObj = entries.map((arr) => {
+    return ([arr[0], arr[1]] = [arr[1], arr[0]]);
+  });
+
+  const result = invertedObj.reduce((acc, curr) => {
+    acc[curr[0]] = curr[1];
+    return acc;
+  }, {});
+
+  return result;
+};
 
 _.invert({ name: 'Sangeeth Sivan', age: 25, 'has pet': true });
