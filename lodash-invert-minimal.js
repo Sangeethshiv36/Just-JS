f9d8f3f5b09eddc4f65027e210fd53422120f5ad
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-10-29 11:51:40 +0530

    lodash-invert-simple skeleton

---
+++ lodash-invert-minimal.js
@@ -0,0 +1,11 @@
+/* lodash _.invert function.
+example:
+invert({'name':'Sangeeth Sivan','age':25,''has pet':true})
+returns: {'Sangeeth Sivan':'name','25':'age,'true':'has pet}
+*/
+
+const _ = {};
+
+_.invert = (obj) => {};
+
+_.invert({ name: 'Sangeeth Sivan', age: 25, 'has pet': true });
