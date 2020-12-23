45f693b1b7b23435801b1d4cba00890f6372a221
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-12-24 02:40:45 +0530

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,12 @@
-// TODO
+const isPalindrome = (word) => {
+  const reversedWord = word.toLowerCase().split('').reverse().join('');
+  if (reversedWord === word.toLowerCase()) {
+    return `The word ${word} is a palindrome!`;
+  } else {
+    return `The word ${word} is not a palindrome!`;
+  }
+};
+
+console.log(isPalindrome('Anna'));
+console.log(isPalindrome('Sangeeth'));
+console.log(isPalindrome('UwU'));
