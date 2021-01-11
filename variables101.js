714b451d48a7abcb271f1340a55da090beb5a2b9
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2021-01-11 13:35:57 +0530

    const vs let hoisting and block scope examples added

--- variables101.js
+++ variables101.js
@@ -3,14 +3,14 @@
 function varScope() {
   glob = 10;
   var loca = 20;
-  console.log('variables created');
+  console.log("variables created");
 }
 
 varScope(); // Execute the function to actually create those variables or use IIFE
 console.log(glob); // 10
 // console.log(loca); // ERROR: Loca is not defined
 
-/* Example : Scope using var.*/
+/* Example 1 : Scope using var.*/
 
 const discountedPrices = (prices, discount) => {
   const clonePrices = prices;
@@ -33,14 +33,83 @@ const discountedPrices = (prices, discount) => {
 
 console.log(discountedPrices([100, 200, 300], 0.5)); //[50,100,150]
 
-/* End of Example */
+/* End of Example1 */
+
+/* Example 2 : Scope using let .*/
+
+const discountedPricesBlockScoped = (prices, discount) => {
+  const clonePrices = prices;
+  let discounted = [];
+  for (let i = 0; i < prices.length; i++) {
+    let discountedPrice = prices[i] * (1 - discount);
+    let finalPrice = Math.round(discountedPrice * 100) / 100;
+    discounted.push(finalPrice);
+  }
+
+  /* Here we get ReferenceError because let is block scoped {} so these varaibled only lives within the for loops block {} */
+
+  /*
+  console.log(i); // i is not defined
+  console.log(discountedPrice); // discountedPrice is not defined
+	console.log(finalPrice); // finalPrice is not defined
+	*/
+
+  return discounted;
+};
+
+console.log(discountedPricesBlockScoped([100, 200, 300], 0.5)); //[50,100,150]
+
+/* End of Example2 */
 
 /* HOISTING - When the javascript interpreter evaluates your code it will move all variable declarations and function declarations to the top of the current scope.*/
 
 console.log(hoisted); // undefined.
 var hoisted;
 
+/* gets interpreted as
+   var hoisted;
+	 console.log(hoisted);
+*/
+
 console.log(fnHoisted()); // HOISTED !
 function fnHoisted() {
-  return 'HOISTED !';
+  return "HOISTED !";
 }
+
+/* For block scoped variable like let it still gets hoisted but when we try to refernece it before the varaible gets declared we get ReferenceError*/
+
+console.log(blockHoisted); // Cannot access "blockHoisted" before initialization
+let blockHoisted;
+
+/* still gets interpreted as
+   let blockHoisted;
+	 console.log(blockHoisted);
+*/
+
+console.log(constablock); // same error
+const constablock = 10; // const variables should always be initialized
+
+console.log(constFnDec()); //same error
+const constFnDec = () => {
+
+}
+
+/* const vs let : Unlike let , once you have assigned a value to const you can't reassign it to a new value */
+
+let name = 'Sangeeth';
+const handle = 'berzerk36'
+
+name = 'Sangeeth Sivan';
+handle = 'newBerz'; // Error : Assignment to constant varibale.
+
+/* Example 3 - using objects in const*/
+const person = {
+	name: 'Sangeeth';
+}
+
+// What i can do
+person.name = 'Sangeeth sivan'; // Because in here i am only changing the property of the const object and not the value of the const variable itself.
+console.log(person.name); //Sangeeth Sivan
+
+// What i can't do
+person = {}; // Error : Assignment to constant varibale.
